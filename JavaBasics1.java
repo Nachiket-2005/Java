2332c88c17af391978a21880b179e9b47e37e203
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-03-09 08:47:29 +0530

    Uploaded File

---
+++ JavaBasics1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class JavaBasics1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // String input = sc.next(); 
+        // System.out.println(input);
+
+        // Input in Java--> nextInt, nextFloat, nextLine, nextDouble, nextByte, nextBoolean,
+        //                  nextShort, nextLong etc.     
+
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // int number = sc.nextInt();
+        // System.out.println(number);
+
+        float price = sc.nextFloat();
+        System.out.println(price);
+    }
+}
