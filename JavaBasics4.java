f41584e62215575578ed85104822e4ba2bedcd23
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-03-09 19:02:10 +0530

    Area of Circle

---
+++ JavaBasics4.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class JavaBasics4 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        float rad = sc.nextFloat();
+        float Area = 3.14f * rad * rad;
+        System.out.println(Area);
+    }
+    
+}
