ce574ec9baff94f3aa1877fe12e2d28193063ee0
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-03-09 19:20:57 +0530

    Type Conversion

---
+++ JavaBasics5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class JavaBasics5{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        float number = sc.nextInt();
+        System.out.println(number);
+    }
+}
