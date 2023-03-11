c755bdb3e1925de4963421d4e70d0091d9eaf665
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-03-11 18:43:05 +0530

    Create JavaBasics10.java

---
+++ JavaBasics10.java
@@ -0,0 +1,23 @@
+public class JavaBasics10 {
+    public static void main(String args[]){
+        int a = 10;
+        int b = ++a;                    //Pre Increament
+        System.out.println(a);
+        System.out.println(b);
+
+        int c = 10;
+        int d = c++;                    //Post Increament
+        System.out.println(c);
+        System.out.println(d);
+
+        int e = 10;
+        int f = --e;                    //Pre Decreament
+        System.out.println(e);
+        System.out.println(f);
+
+        int g = 10;
+        int h = g--;                    //Post Decreament
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
