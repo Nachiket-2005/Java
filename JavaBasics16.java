1572951fd997ed22e9fb285f2f3a88f7c377ab88
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-03-12 12:34:39 +0530

    Create JavaBasics16.java

---
+++ JavaBasics16.java
@@ -0,0 +1,7 @@
+public class JavaBasics16 {
+    public static void main(String args[]){
+        int A = 10;
+        A += 10;
+        System.out.println(A); // you can try also --> -=, *=, /=, %= etc...
+    }
+}
