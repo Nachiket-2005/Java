4300303c3c9a96a3e6966aca92b7ca87b6560e27
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-03-12 12:12:10 +0530

    Create JavaBasics12.java

---
+++ JavaBasics12.java
@@ -0,0 +1,8 @@
+public class JavaBasics12 {
+        public static void main(String args[]){
+            int A = 10;
+            int B = 5;
+            System.out.println((A != B)); // you can also try with this operator by yourself such as >,<,>=,<= etc...
+        }
+    
+}
