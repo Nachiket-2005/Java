33a077f272b576de7e3efe93a08620dc8d787668
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-03-13 18:41:15 +0530

    Create JavaBasics17.java

---
+++ JavaBasics17.java
@@ -0,0 +1,14 @@
+public class JavaBasics17 {
+    public static void main(String args[]){
+
+        int age = 18;
+
+        if(age>=18){
+            System.out.println("Adult : Vote, Drive...");
+        }
+
+        else{
+            System.out.println("Not Adult...");
+        }
+    }
+}
