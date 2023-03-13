fa73eb254e886489e6ced25d06956ee28fd6d349
NACHIKET PRAJAPATI <119723524+Nachiket-2005@users.noreply.github.com>
2023-03-13 18:48:07 +0530

    Create JavaBasics18.java

---
+++ JavaBasics18.java
@@ -0,0 +1,12 @@
+public class JavaBasics18 {
+    public static void main(String args[]){
+        int A = 1, B = 3;
+
+        if(A>B){
+            System.out.println("A is MAX.");
+        }
+        else{
+            System.out.println("B is MAX.");
+        }
+    }
+}
